bca41ebfdfd1edd16523b2b669e6157b4ad8cc77
starrain0273 <starrain0273@imrlab.hoseo.edu>
2018-11-09 12:10:25 +0900

    PP03.GameClass

---
+++ Git/GameFrameWork/Game.h
@@ -0,0 +1,20 @@
+#include "SDL.h"
+#include <iostream>
+
+class Game
+{
+public:
+	Game() {}
+	~Game(){}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void render();
+	void update() {};
+	void handleEnvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Window * m_pWindow;
+	SDL_Renderer* m_pRenderer;
+	bool m_bRunning;
+};
